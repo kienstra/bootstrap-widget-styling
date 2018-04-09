c694a956235929235d9caf8ec66a94a5b583e799
Ryan Kienstra <kienstraryan@gmail.com>
2018-04-09 18:27:52 -0500

    Display date in 'Recent Posts' widget.
    Before, the logic for this was in a JS file.
    But that file is deleted,
    with the logic ported to PHP.
    But I didn't port the date logic.
    So add this, and tests for it.

--- php/widgets/class-bws-widget-recent-comments.php
+++ php/widgets/class-bws-widget-recent-comments.php
@@ -28,12 +28,11 @@ class BWS_Widget_Recent_Comments extends \WP_Widget_Recent_Comments {
 		foreach ( $dom->getElementsByTagName( 'li' ) as $li ) {
 			$anchor = $li->getElementsByTagName( 'a' );
 			if ( $anchor->length > 0 ) {
-				$last_index  = $anchor->length - 1;
-				$list_group .= sprintf( '<a href="%s" class="list-group-item">%s</a>', esc_url( $anchor->item( $last_index )->attributes->getNamedItem( 'href' )->nodeValue ), esc_html( $li->textContent ) ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.NotSnakeCaseMemberVar
+				$list_group .= sprintf( '<a href="%s" class="list-group-item">%s</a>', esc_url( $anchor->item( $anchor->length - 1 )->attributes->getNamedItem( 'href' )->nodeValue ), esc_html( $li->textContent ) ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.NotSnakeCaseMemberVar
 			}
 		}
 		$list_group .= '</div>';
-		echo preg_replace( '/<ul.*<\/ul>/', $list_group, $output ); // WPCS: XSS ok.
+		echo preg_replace( '/<ul[\s\S]*<\/ul>/', $list_group, $output ); // WPCS: XSS ok.
 	}
 
 }
--- tests/php/widgets/test-class-bws-widget-recent-posts.php
+++ tests/php/widgets/test-class-bws-widget-recent-posts.php
@@ -38,24 +38,44 @@ class Test_BWS_Widget_Recent_Posts extends \WP_UnitTestCase {
 	public function test_widget() {
 		$first_post  = $this->factory()->post->create_and_get();
 		$second_post = $this->factory()->post->create_and_get();
+		$args        = array(
+			'before_title'  => '',
+			'after_title'   => '',
+			'before_widget' => '',
+			'after_widget'  => '',
+		);
+
 		ob_start();
 		$this->instance->widget(
+			$args,
 			array(
-				'before_title'  => '',
-				'after_title'   => '',
-				'before_widget' => '',
-				'after_widget'  => '',
-			),
-			array(
-				'count' => 1,
+				'count' => 5,
 			)
 		);
 		$output = ob_get_clean();
-		$this->assertEquals( 0, strpos( $output, '<div class="list-group">' ) );
-		$this->assertContains( '<a class="list-group-item"', $output );
+
+		$this->assertNotFalse( strpos( $output, '<div class="list-group">' ) );
+		$this->assertContains( 'class="list-group-item"', $output );
 		$this->assertContains( $first_post->post_title, $output );
 		$this->assertContains( $second_post->post_title, $output );
 		$this->assertNotContains( '<ul', $output );
+		$this->assertNotContains( '<span class="post-date label label-primary pull-right', $output );
+
+		// Test for when the 'Display post date?' checkbox is checked.
+		ob_start();
+		$this->instance->widget(
+			$args,
+			array(
+				'show_date' => true,
+				'count'     => 5,
+			)
+		);
+		$output = ob_get_clean();
+
+		$this->assertContains( '<span class="post-date label label-primary pull-right', $output );
+		$this->assertNotFalse( strpos( $output, '<div class="list-group">' ) );
+		$this->assertContains( $first_post->post_title, $output );
+		$this->assertContains( $second_post->post_title, $output );
 	}
 
 }
