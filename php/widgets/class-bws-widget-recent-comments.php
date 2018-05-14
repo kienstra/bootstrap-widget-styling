544e75a02eb2ffbc55aab64b9f3b80de27d8de95
Ryan Kienstra <kienstraryan@gmail.com>
2018-05-13 19:30:05 -0500

    Ensure that ->nodeValue isn't empty before accessing it.
    Similar to the previous commit,
    prevent a possible PHP warning.

--- php/widgets/class-bws-widget-recent-comments.php
+++ php/widgets/class-bws-widget-recent-comments.php
@@ -27,7 +27,7 @@ class BWS_Widget_Recent_Comments extends \WP_Widget_Recent_Comments {
 		$list_group = '<div class="list-group">';
 		foreach ( $dom->getElementsByTagName( 'li' ) as $li ) {
 			$anchor = $li->getElementsByTagName( 'a' );
-			if ( $anchor->length > 0 ) {
+			if ( ! empty( $anchor->item( $anchor->length - 1 )->attributes->getNamedItem( 'href' )->nodeValue ) ) {
 				$list_group .= sprintf( '<a href="%s" class="list-group-item">%s</a>', esc_url( $anchor->item( $anchor->length - 1 )->attributes->getNamedItem( 'href' )->nodeValue ), esc_html( $li->textContent ) ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.NotSnakeCaseMemberVar
 			}
 		}
